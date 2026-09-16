b06063e2b152c86aff055f4f35bc1a184a8260e2
Linnea Nordli <linneasnordli@gmail.com>
2026-09-15 15:52:58 -1000

    updating script from lecture Sept.15th

--- Week_04/Scripts/week4_inclass_script.R
+++ Week_04/Scripts/week4_inclass_script.R
@@ -1,23 +1,25 @@
-######## In-Class Assignment: intro to dplyr ########
+######## Notes from In-Class Lecture: intro to dplyr ########
 ### Created by: Linnea Nordli
 ### Created on: 2026-09-15
 ### Week 4
 ####################################
 
-## load libraries
+### Load libraries ###
 library(palmerpenguins)
 library(tidyverse)
 library(here)
 
-## Load data and data analysis ##
+### Load data and data analysis ###
 # dataframe is in palmerpenguin package
 glimpse(penguins)
 head(penguins) # look at variables in dataframe
 
-## Filter the dataframe in different ways using FILTER function ##
+### Data wrangling: intro to dplyr ###
+## FILTER ##
+# Filter the dataframe in different ways using FILTER function ##
 filter(penguins, sex=="female") # filters for ONLY female penguins
 
-filter(penguins, year==2008) # filter penguins only measured in the year 2008, working w numbers don't need quotes
+filter(penguins, year==2008) # filter penguins only measured in the year 2008
 
 filter(penguins, body_mass_g>5000) #penguins with body mass greater than 5000g
 
@@ -25,19 +27,23 @@ filter(penguins, sex=="female", body_mass_g>5000) # females also greater than 50
 
 filter(penguins, year==2008 | year==2009) # collected in EITHER 2008 or 2009
 penguins|>
-  filter(year %in% c(2008,2009)) #same filtering using piping and %in% and grouping
+  filter(year %in% c(2008,2009)) #same but filtering using piping, %in% and grouping
 
 filter(penguins, island!="Dream") # penguins NOT from the island Dream
 
 filter(penguins, species %in% c("Adelie","Gentoo")) # penguins in the species Adelie and Gentoo 
 filter(penguins, species == "Adelie" | species=="Gentoo") # or lke this
 
-## using the MUTATE function to add new columns ##
+## MUTATE ##
+# using the MUTATE function to add new columns and keep existing##
 mutate(.data = penguins,
        body_mass_kg = body_mass_g / 1000) # add a new column converting body mass in g to kg
 mutate(.data = penguins,
        body_mass_kg = body_mass_g / 1000,
        bill_length_depth = bill_length_mm / bill_depth_mm) # change multiple columns at once
+# mutate with across(), apply the same transformation across multiple columns at once
+penguins |>
+  mutate(across(where(is.numeric), ~ round(.x, 1))) #For every numeric column, round its values to 1 decimal place
 # mutate with IF_ELSE
 mutate(.data = penguins,
        after_2008 = if_else(year > 2008, "After 2008", "Before 2008")) # mutate with IF_ELSE
@@ -59,39 +65,56 @@ penguins |>
 penguins |>
   drop_na(sex) |>
   ggplot(aes(x = sex, y = flipper_length_mm)) +
-  geom_boxplot()
+  geom_boxplot() # Drop NAs from sex, and then plot boxplots of flipper length by sex.
 
-## Select certain columns to remain in the dataframe using SELECT function ##
- 
-## SUMMARISE
-# create new tables
+## SELECT ## 
+# Select certain columns to remain in the dataframe using SELECT function ##
 penguins |>
-  summarise(mean_flipper = mean(flipper_length_mm, na.rm = TRUE))
+  filter(sex == "female") |>
+  mutate(log_mass = log(body_mass_g)) |>
+  select(species, island, sex, log_mass)
+# You can also use select() to rename columns
+penguins |>
+  filter(sex == "female") |>
+  mutate(log_mass = log(body_mass_g)) |>
+  select(Species = species, island, sex, log_mass) #Here, we are renaming species to have a capital S.
+
+## ARRANGE ## 
+#Use arrange() to sort rows by a column
+penguins |>
+  arrange(body_mass_g) #ascending by default
+# if want descending order use desc()
+penguins |>
+  arrange(desc(body_mass_g))
+
+## SUMMARISE ##
+# create new tables of summarized data
+penguins |>
+  summarise(mean_flipper = mean(flipper_length_mm, na.rm = TRUE)) #Calculate the mean flipper length (and exclude any NAs).
 
 penguins |>
   summarise(mean_flipper = mean(flipper_length_mm, na.rm = TRUE),
-            min_flipper  = min(flipper_length_mm, na.rm = TRUE))
-# use group_by within summarise()
+            min_flipper  = min(flipper_length_mm, na.rm = TRUE)) #Calculate mean and min flipper length
+
+# use group_by within summarise() to summarize values by certain groups
 penguins |>
   group_by(island) |>
   summarise(mean_bill_length = mean(bill_length_mm, na.rm = TRUE),
             max_bill_length  = max(bill_length_mm, na.rm = TRUE),
-            n                = n())
-# group by both and sex
+            n                = n()) #calculates the mean, max, and count of bill length by island
+
 penguins |>
   group_by(island, sex) |>
   summarise(mean_bill_length = mean(bill_length_mm, na.rm = TRUE),
-            max_bill_length  = max(bill_length_mm, na.rm = TRUE))
-
+            max_bill_length  = max(bill_length_mm, na.rm = TRUE)) ## group by both and sex
 
 ## COUNTS ##
 #count() is a quick shortcut for counting rows per group — no need for group_by() + summarise().
 penguins |>
   count(species)
-# multiple variables
+# multiple variables at once
 penguins |>
   count(species, island)
-
 
 ## REMOVE NAs ##
 # drop_na() drops rows with NAs from specific column
@@ -102,8 +125,3 @@ penguins |>
   drop_na(sex) |>
   group_by(island, sex) |>
   summarise(mean_bill_length = mean(bill_length_mm, na.rm = TRUE))
-
-
-# dad joke package
-library(dadjokeapi)
-groan() # dad joke package
